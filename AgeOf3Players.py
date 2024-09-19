4763fab1afc74989c68b5fe10b7528c787ee6b76
technical1100 <technical110011@gmail.com>
2024-09-19 01:23:25 -0700

    Create AgeOf3Players.py

---
+++ AgeOf3Players.py
@@ -0,0 +1,31 @@
+'''It were the days of domination from the traditional metros in the team selections and everytime the team is announced for the Indian Squad, mere disappointment was left with this small town player. Dhoni’ill fate continued even during the team selections for the India A squad to tour to Zimbabwe.3 new players from Mumbai were on the list for the Indian team and it was claimed by the selectors that Dhoni was a bit younger than the 3 selected players.
+  Assume the 3 players are Named X,Y and Z. The ages of the players X and Y are the same and the age of the Z is 10 more than other 2 players. Given the total age of the 3 players, find the age of the 3 players.
+Input format:
+First line of the input is an integer that corresponds to the total age of the 3 players.
+Output format:
+Output should display the ages of the three players in 3 lines. The age of the eldest player should be displayed in the last line.
+Sample input and output 1:
+70
+20
+20
+30
+Sample input and output 2:
+100
+30
+30
+40
+'''
+# Input the total age of the three players
+total_age = int(input())
+
+# Calculate the ages of players X and Y
+age_X_Y = (total_age - 10) // 2
+
+# Calculate the age of player Z
+age_Z = age_X_Y + 10
+
+# Output the ages of the three players
+print(age_X_Y)
+print(age_X_Y)
+print(age_Z)
+
